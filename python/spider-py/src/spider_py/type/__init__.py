f6826c47f359f7be6d12724025e850f910f0f47a
LinZhihao-723 <pleiades3190@gmail.com>
2026-04-13 21:50:08 -0400

    Change Py.

--- python/spider-py/src/spider_py/type/__init__.py
+++ python/spider-py/src/spider_py/type/__init__.py
@@ -1,5 +1,6 @@
 """Spider type package."""
 
+# Change
 from spider_py.type.tdl_convert import to_tdl_type_str
 from spider_py.type.tdl_parse import parse_tdl_type
 from spider_py.type.type import Double, Float, Int8, Int16, Int32, Int64
